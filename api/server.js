20e5061f014636e806d8743c3ea9b17d685b8e2f
datngovan <s3817813@rmit.edu.vn>
2023-12-20 15:03:54 +0700

    feat: update

--- api/server.js
+++ api/server.js
@@ -17,11 +17,10 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
+//Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
